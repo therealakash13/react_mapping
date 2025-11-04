1b9f6bb9003c9fe5b0487d6782b67a7f7f400d98
Akash <therealakash13@gmail.com>
2025-11-04 13:58:43 +0530

    emojipedia

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import emojipedia from "../emojipedia";
+import Entry from "./Entry";
+
+function App() {
+  function createCard(entry) {
+    return (
+      <Entry
+        key={entry.id}
+        id={entry.id}
+        emoji={entry.emoji}
+        name={entry.name}
+        meaning={entry.meaning}
+      />
+    );
+  }
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+    </div>
+  );
+}
+
+export default App;
